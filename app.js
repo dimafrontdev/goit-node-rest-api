298abac1ab005efcae08ee2a462fb58addc9f427
Dima <dima@vantage.software>
2025-04-06 15:12:58 +0300

    fixed validation

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
